8d12ef2326ba17d5f253c8f08cb6f9e6858c56ff
rahdek11 <dickyyudiana88@gmail.com>
2020-09-02 16:29:27 +0800

    edit config

--- app/config/config.php
+++ app/config/config.php
@@ -16,3 +16,5 @@ define('CSS', 'http://127.0.0.1/wedding/public/assets/css');
 define('IMAGES', 'http://127.0.0.1/wedding/public/assets/images');
 define('JS', 'http://127.0.0.1/wedding/public/assets/js');
 define('BASEURL', 'http://127.0.0.1/wedding/public');
+
+// Some code
