defde9d8e3a1e7840faaca7d995e62eaeafc948f
rahdek11 <dickyyudiana88@gmail.com>
2020-10-07 12:36:42 +0800

    change some codes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,19 +2,15 @@ $(document).ready(function () {
 
     AOS.init();
 
-    // Set the date we're counting down to
     var countDownDate = new Date("Jan 01, 2021 08:00:00").getTime();
 
-    // Update the count down every 1 second
+
     var x = setInterval(function () {
 
-        // Get today's date and time
         var now = new Date().getTime();
 
-        // Find the distance between now and the count down date
         var distance = countDownDate - now;
 
-        // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
